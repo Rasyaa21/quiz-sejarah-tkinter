5507e4ed21ff8f12781098824cc768ab026de600
Rasyaa21 <naufalrasya21907@gmail.com>
2024-11-28 19:52:59 +0700

    first commit

---
+++ quiz_data.py
@@ -0,0 +1,113 @@
+quiz_data = [
+    {
+        "image_path": "",
+        "question": "Apa yang menjadi alasan utama peristiwa Bandung Lautan Api?",
+        "choices": [
+            "Serangan Jepang ke Bandung", 
+            "Ultimatum dari tentara Sekutu untuk mengosongkan kota", 
+            "Perebutan wilayah oleh rakyat dan Belanda", 
+            "Kekurangan sumber daya di Bandung"
+            ],
+        "answer": "Ultimatum dari tentara Sekutu untuk mengosongkan kota"
+    },
+    {
+        "image_path": "",
+        "question": "Kapan tepatnya peristiwa Bandung Lautan Api terjadi?",
+        "choices": [
+            "10 Maret 1946", 
+            "17 Agustus 1945", 
+            "24 Maret 1946", 
+            "1 Januari 1946"
+            ],
+        "answer": "24 Maret 1946"
+    },
+    {
+        "image_path": "img/Muhammad_Toha.jpg",
+        "question": "Foto diatas adalah tokoh yang memimpin pembakaran kota Bandung dalam peristiwa Bandung Lautan Api adalah",
+        "choices": [
+            "Mohammad Toha",
+            "Sutan Sjahrir",
+            "Ir. Soekarno",
+            "Abdul Haris Nasution"
+            ],
+        "answer": "Mohammad Toha"
+    },
+    {
+        "image_path": "",
+        "question": "Apa tujuan dari pembakaran kota Bandung oleh para pejuang?",
+        "choices": [
+            "Memprovokasi Sekutu",
+            "Menghindari penguasaan kota oleh Sekutu dan Belanda",
+            "Menguji taktik perang bumi hangus",
+            "Mengusir penduduk Bandung"
+        ],
+        "answer": "Mengusir penduduk Bandung"
+    },
+    {
+        "image_path": "",
+        "question": "Monumen Bandung Lautan Api dibangun untuk mengenang peristiwa ini. Berapa tingginya?",
+        "choices": [
+            "20 meter",
+            "45 meter",
+            "50 meter",
+            "75 meter"
+        ],
+        "answer": "45 meter"
+    },
+    {
+        "image_path": "",
+        "question": "⁠Tokoh yang memimpin pembakaran kota Bandung dalam peristiwa Bandung Lautan Api adalah",
+        "choices": [
+            "Soekarno",
+            "Mohammad Toha",
+            "Jendral Sudirman",
+            "Abdul Haris Nasution"
+        ],
+        "answer": "Mohammad Toha"
+    },
+    {
+        "image_path": "",
+        "question": "Tujuan utama pembakaran kota Bandung oleh pejuang Indonesia dalam peristiwa Bandung Lautan Api adalah",
+        "choices": [
+            "Menghindari penguasaan kota oleh Sekutu",
+            "Membalas serangan yang dilakukan oleh Sekutu",
+            "Memindahkan pusat pemerintahan Indonesia",
+            "Memindahkan pusat pemerintahan Indonesia"
+        ],
+        "answer": "Menghindari penguasaan kota oleh Sekutu"
+    },
+    {
+        "image_path": "img/bandung_api.jpeg",
+        "question": "⁠Dalam peristiwa Bandung Lautan Api, bagian kota Bandung yang dibakar oleh pejuang Indonesia adalah",
+        "choices": [
+            "Bandung Utara",
+            "Bandung Timur",
+            "Bandung Selatan",
+            "Bandung Barat"
+        ],
+        "answer": "Bandung Selatan"
+    },
+
+    {
+        "image_path": "",
+        "question": "⁠Sebelum peristiwa Bandung Lautan Api, Sekutu memberikan ultimatum kepada rakyat Bandung untuk meninggalkan wilayah selatan dalam waktu",
+        "choices": [
+            "24 jam",
+            "48 jam",
+            "72 jam",
+            "1 minggu"
+        ],
+        "answer": "48 jam"
+    },
+    {
+        "image_path": "",
+        "question": "⁠Salah satu tokoh yang berjasa dalam mengoordinasikan pembakaran kota Bandung adalah",
+        "choices": [
+            "Kolonel A.H. Nasution",
+            "Laksamana Maeda",
+            "Sutan Sjahrir",
+            "Kapten Aruji Kartawinata"
+        ],
+        "answer": "Kolonel A.H. Nasution"
+    },
+]
